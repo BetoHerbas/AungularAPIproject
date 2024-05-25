1b1436a8ab89cb574ea09f08b1dfafb98649723b
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-25 19:43:05 -0400

    Adding pages and routes with a menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ControlPanelComponent } from './pages/control-panel/control-panel.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'store', component: ShopComponent},
+    {path: 'details/:id', component: ProductDetailComponent},
+    {path: 'admin', component: ControlPanelComponent},
+];
