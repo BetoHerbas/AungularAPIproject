9d5856930d86b84c09767fd27f8b2bbec4111675
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-26 14:41:23 -0400

    Changing the products displaying into a table for administartor view

--- src/app/pages/control-panel/control-panel.component.ts
+++ src/app/pages/control-panel/control-panel.component.ts
@@ -1,11 +1,16 @@
 import { Component, inject } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+
 import { Product } from '../../interfaces/product';
 import { ProductService } from '../../services/product.service';
 import { ProductComponent } from '../../elements/product/product.component';
@@ -16,29 +21,34 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 @Component({
   selector: 'app-control-panel',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, MatFormFieldModule, MatInputModule,MatSelectModule, MatCardModule, ProductComponent, ReactiveFormsModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule, ProductComponent, ReactiveFormsModule, MatTableModule, MatSortModule, MatPaginatorModule],
   templateUrl: './control-panel.component.html',
   styleUrl: './control-panel.component.scss'
 })
 export class ControlPanelComponent {
   productList: Product[] = [];
+  dataSource = new MatTableDataSource<Product>(this.productList);
   productService: ProductService = inject(ProductService);
-  displayedColumns: string[] = ['id', 'title', 'price', 'description', 'category', 'image'];
+  displayedColumns: string[] = ['id', 'title', 'price', 'description', 'category', 'image', 'edit', 'delete'];
 
   productForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.productForm = this.fb.group({
-      title: [''],
+      title: ['', Validators.required],
       price: [0, Validators.min(0)],
-      description: [''],
-      category: [''],
-      image: ['']
+      description: ['', Validators.required],
+      category: ['', Validators.required],
+      image: ['', Validators.required]
     });
   }
 
-  ngOnInit():void {
-    this.productService.getAllProducts().then((productList: Product[]) => this.productList = productList);
+  ngOnInit(): void {
+    this.productService.getAllProducts().then((productList: Product[]) => {
+      this.productList = productList
+      this.dataSource.data = productList
+    });
+
   }
 
   hide = true;
@@ -52,6 +62,7 @@ export class ControlPanelComponent {
       const newProduct: Product = this.productForm.value;
       const createdProduct = await this.productService.createProduct(newProduct);
       this.productList.push(createdProduct);
+      this.dataSource.data = this.productList;
       this.productForm.reset();
     }
     else {
@@ -59,11 +70,8 @@ export class ControlPanelComponent {
     }
   }
   async removeProduct(productId: number) {
-    try {
-      await this.productService.deleteProduct(productId);
-      this.productList = this.productList.filter(product => product.id !== productId);
-    } catch (error) {
-      console.error('Error deleting product:', error);
-    }
+    await this.productService.deleteProduct(productId);
+    this.productList = this.productList.filter(product => product.id !== productId);
+    this.dataSource.data = this.productList;
   }
 }
