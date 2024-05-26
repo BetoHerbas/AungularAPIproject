955e80a4a2d6f72672947e67a3821ae48530865f
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-26 17:43:39 -0400

    Adding dialog to create a product

--- src/app/pages/control-panel/control-panel.component.ts
+++ src/app/pages/control-panel/control-panel.component.ts
@@ -1,11 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCardModule } from '@angular/material/card';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
@@ -17,30 +12,24 @@ import { ProductService } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CreateProductDialogComponent } from '../../elements/create-product-dialog/create-product-dialog.component';
+
 
 @Component({
   selector: 'app-control-panel',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCardModule, ReactiveFormsModule, MatTableModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTableModule],
   templateUrl: './control-panel.component.html',
   styleUrl: './control-panel.component.scss'
 })
 export class ControlPanelComponent {
   productList: Product[] = [];
-  dataSource = new MatTableDataSource<Product>(this.productList);
   productService: ProductService = inject(ProductService);
+
+  dataSource = new MatTableDataSource<Product>(this.productList);
   displayedColumns: string[] = ['id', 'title', 'price', 'description', 'category', 'image', 'edit', 'delete'];
 
-  productForm: FormGroup;
-
-  constructor(private fb: FormBuilder, private dialog: MatDialog, private _snackBar: MatSnackBar) {
-    this.productForm = this.fb.group({
-      title: ['', Validators.required],
-      price: [0, Validators.min(0)],
-      description: ['', Validators.required],
-      category: ['', Validators.required],
-      image: ['', Validators.required]
-    });
+  constructor(private dialog: MatDialog, private _snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
@@ -57,19 +46,18 @@ export class ControlPanelComponent {
     event.stopPropagation();
   }
 
-  async addProduct() {
-    if (this.productForm.valid) {
-      const newProduct: Product = this.productForm.value;
-      const createdProduct = await this.productService.createProduct(newProduct);
-      this.productList.push(createdProduct);
-      this.dataSource.data = this.productList;
-      this.productForm.reset();
-      this.showSnackBar('Product added successfully');
-    }
-    else {
-      alert('Please fill out all fields');
-    }
+  openCreateProductDialog(): void{
+    const dialogRef = this.dialog.open(CreateProductDialogComponent);
+    dialogRef.afterClosed().subscribe(async (result: Product) => {
+      if (result) {
+        const createdProduct = await this.productService.createProduct(result);
+        this.productList.push(createdProduct);
+        this.dataSource.data = this.productList;
+        this.showSnackBar('Product added successfully');
+      }
+    });
   }
+
   async removeProduct(productId: number) {
     const dialogRef = this.dialog.open(ConfirmDialogComponent);
 
@@ -82,10 +70,13 @@ export class ControlPanelComponent {
       }
     });
   }
+
   showSnackBar(mensaje: string) {
     this._snackBar.open(mensaje, 'Close', {
       duration: 3000
     });
   }
+
+
   
 }
