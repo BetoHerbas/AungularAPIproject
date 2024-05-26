b502e405fbffdd095b8623e19b68e61e9f33ebd4
Beto Herbas <betitoherbas@gmail.com>
2024-05-26 19:43:44 -0400

    Adding sections to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductComponent } from '../../elements/product/product.component';
+import { Product } from '../../interfaces/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ ProductComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  productList: Product[] = [];
+  productService: ProductService = inject(ProductService);
+  displayedColumns: string[] = ['id', 'title', 'price', 'description', 'category', 'image'];
 
+  ngOnInit():void {
+    this.productService.getAllProducts().then((productList: Product[]) => {
+      this.productList = productList.slice(0, 5);
+    });
+  }
 }
