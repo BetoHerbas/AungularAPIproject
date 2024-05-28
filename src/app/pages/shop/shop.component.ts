7765159ea30ad62a68e5193bafbce7a5e8249dc5
Beto Herbas <betitoherbas@gmail.com>
2024-05-27 22:47:57 -0400

    Shop page finish

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductComponent } from '../../elements/product/product.component';
+import { FooterComponent } from '../../elements/footer/footer.component';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../interfaces/product';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
+  imports: [ ProductComponent, FooterComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
 export class ShopComponent {
+  productList: Product[] = [];
+  productService: ProductService = inject(ProductService);
 
+  ngOnInit():void {
+    this.productService.getAllProducts().then((productList: Product[]) => {
+      this.productList = productList;
+    });
+  }
 }
