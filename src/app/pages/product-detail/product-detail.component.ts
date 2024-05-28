0d248b57c4e90d15622236c1cf35895255b1f30c
Beto Herbas <betitoherbas@gmail.com>
2024-05-28 14:54:28 -0400

    Adding footer to diferent pages

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { Product } from '../../interfaces/product';
 import { ProductService } from '../../services/product.service';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { FooterComponent } from '../../elements/footer/footer.component';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss'],
   standalone: true,
-  imports: [MatGridListModule,MatButtonModule,MatCardModule],
+  imports: [MatGridListModule,MatButtonModule,MatCardModule, FooterComponent],
 })
 export class ProductDetailComponent implements OnInit {
   product!: Product;
   quantity: number = 1;
 
   constructor(
+    private renderer: Renderer2,
     private route: ActivatedRoute,
     private router: Router,
     private productService: ProductService
   ) {}
 
   ngOnInit() {
+    this.renderer.setProperty(document.documentElement, 'scrollTop', 0);
     this.route.paramMap.subscribe(async params => {
       const productId = +params.get('id')!;
       this.product = await this.productService.getProductById(productId);
