6665ca4ff9650af13e92216b97036799fb7a3fdf
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-28 14:42:20 -0400

    Adding styles of css to cart component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
