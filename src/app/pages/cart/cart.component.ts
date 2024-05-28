bcd76f3ebd7c914ce6d447d5f0ab81f84f262dfc
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-28 14:26:20 -0400

    adding remove from cart function and show total

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -4,12 +4,13 @@ import { CartService } from '../../services/cart.service';
 
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatIconModule],
+  imports: [CommonModule, MatTableModule, MatIconModule, CurrencyPipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
@@ -18,12 +19,43 @@ export class CartComponent {
   cartService: CartService = inject(CartService);
 
   dataSource = new MatTableDataSource<Cart>(this.productsCartList);
-  displayedColumns: string[] = ['delete','image','title', 'price', 'quantity', 'subtotal'];
+  displayedColumns: string[] = ['delete', 'image', 'title', 'price', 'quantity', 'subtotal'];
+  total = 0;
+  totalDataSource = new MatTableDataSource<any>;
+
+  constructor(private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.cartService.getAllProducts().then((productsCartList: Cart[]) => {
       this.productsCartList = productsCartList;
       this.dataSource.data = productsCartList;
+      this.calculateTotal();
+    });
+
+  }
+
+  getTotal(): number {
+    let total = 0;
+    this.productsCartList.forEach((cartItem: Cart) => {
+      total += cartItem.subtotal;
+    });
+    return total;
+  }
+  calculateTotal() {
+    this.total = this.getTotal();
+    this.totalDataSource = new MatTableDataSource([{ total: this.total }]);
+  }
+
+  async removeFromCart(productId: number) {
+    const result = await this.cartService.deleteFromCart(productId);
+      this.productsCartList = this.productsCartList.filter(product => product.id !== productId);
+      this.dataSource.data = this.productsCartList;
+      this.calculateTotal();
+      this.showSnackBar('Removed from cart successfully');
+  }
+  showSnackBar(message: string) {
+    this._snackBar.open(message, 'Close', {
+      duration: 3000
     });
   }
 }
