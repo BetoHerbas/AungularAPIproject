542939cd48da0755ab593ac4ee91c5aec1dff967
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-06-17 19:54:35 -0400

    Modifying some styles

--- src/app/pages/buy-category/buy-category.component.ts
+++ src/app/pages/buy-category/buy-category.component.ts
@@ -16,10 +16,12 @@ export class BuyCategoryComponent {
   productList: Product[] = [];
   productService: ProductService = inject(ProductService);
   route: ActivatedRoute = inject(ActivatedRoute);
+  category: string | null= '';
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const category = params.get('category');
+      this.category = category;
       if (category) {
         this.productService.getAllProducts().then((productList: Product[]) => {
           this.productList = productList.filter(product => product.category.toLowerCase() === category.toLowerCase());
