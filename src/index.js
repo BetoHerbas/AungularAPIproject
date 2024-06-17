8fa96e54fd387d1b9cc505a65c64d39d2105e4ae
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-06-17 10:31:00 -0400

    Using db for shop by category

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import  usersRoute  from "./routes/users.routes.js";
 import productsRoute  from "./routes/products.routes.js";
 import buycartRoute from "./routes/buycart.routes.js";
+import categoriesRoute from "./routes/categories.routes.js";
 import express from 'express';
 import cors from 'cors';
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(usersRoute)
 app.use(productsRoute)
 app.use(buycartRoute)
+app.use(categoriesRoute)
 app.listen(3000);
 console.log('Server running on 300');
